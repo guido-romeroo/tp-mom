f855dea3fd20a199b0be90aa427f20ac31051c5b
guido <gromerok@fi.uba.ar>
2026-09-15 10:58:37 -0300

    feat: se agrega la funcionalidad de las working queues con su suite de tests pasando, falta aclarar aspectos de requeue y publisher confirms, así como también confirmar los casos de error si están correctos

--- python/src/common/middleware/middleware_rabbitmq.py
+++ python/src/common/middleware/middleware_rabbitmq.py
@@ -1,12 +1,52 @@
 import pika
 import random
 import string
-from .middleware import MessageMiddlewareQueue, MessageMiddlewareExchange
+from .middleware import MessageMiddlewareCloseError, MessageMiddlewareDisconnectedError, MessageMiddlewareMessageError, MessageMiddlewareQueue, MessageMiddlewareExchange
+from pika.exceptions import ChannelClosed, UnroutableError, NackError
 
 class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
 
     def __init__(self, host, queue_name):
-        pass
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=host))
+        self.channel = self.connection.channel()
+        self.channel.confirm_delivery() # publisher confirms
+
+        self.queue_name = queue_name
+        self.channel.queue_declare(queue=queue_name, durable=True) # queues durables para resistir caídas del broker
+
+    def close(self):
+        try:
+            self.connection.close()
+        except:
+            raise MessageMiddlewareCloseError()
+
+    def send(self, message):
+        if self.channel.is_closed or self.connection.is_closed:
+            raise MessageMiddlewareDisconnectedError()
+        try:
+            self.channel.basic_publish(exchange='', routing_key=self.queue_name, body=message, properties=pika.BasicProperties(delivery_mode=2)) # delivery_mode=2 para persistir los mensajes
+        except Exception as e:
+            raise MessageMiddlewareMessageError(e)
+
+        
+    def start_consuming(self, on_message_callback):
+        def callback(ch, method, properties, body):
+            on_message_callback(body, lambda: ch.basic_ack(delivery_tag=method.delivery_tag), lambda: ch.basic_nack(delivery_tag=method.delivery_tag, requeue=True))
+            # preguntar en clase si se debe usar requeue en las working queues
+
+        self.channel.basic_consume(queue=self.queue_name, on_message_callback=callback) 
+
+        try:
+            self.channel.start_consuming()
+        except ChannelClosed:
+            raise MessageMiddlewareDisconnectedError()
+        except Exception as e:
+            raise MessageMiddlewareMessageError(e)
+
+    def stop_consuming(self):
+        if self.channel.is_closed or self.connection.is_closed:
+            raise MessageMiddlewareDisconnectedError()
+        self.channel.stop_consuming()
 
 class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
     
